4f1853109b61a58a16a7ae168fae3ee030762048
Daan Sieben <developer@daansieben.net>
2020-12-01 07:17:07 +0100

    Day 01

--- TS/01/index.ts
+++ TS/01/index.ts
@@ -4,6 +4,30 @@ export class PuzzleSolution extends BasePuzzle implements Puzzle {
     public run() {
         const result: Result = {};
 
+        const records: number[] = this.getInputAsRows().map(v => parseFloat(v));
+
+        const totalNumber = 2020;
+
+        records.forEach(valueA => {
+            if (result.a && result.b) { return; }
+            if (valueA > totalNumber) { return; }
+
+            records.forEach(valueB => {
+                if (result.a && result.b) { return; }
+                if (valueA + valueB > totalNumber) { return; }
+
+                if (valueA + valueB === totalNumber) {
+                    result.a = valueA * valueB;
+                }
+                
+                records.forEach(valueC => {
+                    if (valueA + valueB + valueC === totalNumber) {
+                        result.b = valueA * valueB * valueC;
+                    } 
+                })
+            })
+        })
+
         return result;
     }
 
